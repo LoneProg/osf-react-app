1a84f650e09ba8473dd2abf77a40f3876514fbef
loneprog <wolfdon73@gmail.com>
2025-02-13 13:50:40 +0000

    Add navigation links and toggle functionality to Header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,45 @@
 import React from 'react';
-import './Header.css';
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  const { NavOpen, setNavOpen } = useState(false);
+  const handleNavToggle = () => {
+    setNavOpen(!NavOpen);
+  };
+  const handleNavLinkClick = () => {
+    setNavOpen(false);
+  };
+  return (
+    <header>
+      <nav className={NavOpen ? "open" : ""}>
+        <ul>
+          <li>
+            <NavLink to="/" onClick={handleNavLinkClick}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" onClick={handleNavLinkClick}>
+              About Us
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/services" onClick={handleNavLinkClick}>
+              Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contacts" onClick={handleNavLinkClick}>
+              Contacts
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <button onClick={handleNavToggle}>Toggle Nav</button>
+    </header>
+  );
+};
 
 const Navbar = () => {
   return (
